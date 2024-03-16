df5a97a25e5ef100b0da9023db793bd984d611b0
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2024-03-16 22:01:16 +0000

    feat: define types for api
    - add types for data returned from api

---
+++ src/types/types.ts
@@ -0,0 +1,18 @@
+export interface Employee {
+  firstName: string;
+  lastName: string;
+  id: string;
+}
+
+export interface Absence {
+  id: number;
+  startDate: string;
+  days: number;
+  absenceType: string;
+  employee: Employee;
+  approved: boolean;
+}
+
+export interface AbsenceWithConflict extends Absence {
+  conflicts: boolean;
+}
